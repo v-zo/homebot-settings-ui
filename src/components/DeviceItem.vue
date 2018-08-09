cab8b6c0094493d631de8b462e5092ea90ada5a8
Vasya Zo <vasyaweb.zo@gmail.com>
2018-08-09 18:23:45 +0500

    Devices

---
+++ src/components/DeviceItem.vue
@@ -0,0 +1,59 @@
+<template>
+    <v-container fluid grid-list-xl>
+        <v-layout row justify-space-between>
+
+            <v-flex xs12>
+                <v-card color="blue" class="white--text">
+                    <v-layout row>
+                        <v-flex xs10 pa-0 ma-0>
+                            <v-card-title primary-title>
+                                <div>
+                                    <div class="headline">Device</div>
+                                    <div>description</div>
+                                    
+                                </div>
+                            </v-card-title>
+                        </v-flex>
+                        <v-flex xs2 align-center >
+                            <v-btn class="forget-btn">
+                                forget
+                            </v-btn>
+                        </v-flex>
+                    </v-layout>
+                    <v-divider light></v-divider>
+                    <v-card-actions class="pa-3">
+                        (status)
+                        <v-spacer></v-spacer>
+                        forget
+                    </v-card-actions>
+                </v-card>
+            </v-flex>
+
+        </v-layout>
+    </v-container>
+</template>
+
+<script>
+
+
+export default {
+  name: 'DeviceItem',
+  props: {
+    
+  },
+  components: {
+    // CircleNav
+  }
+}
+</script>
+
+<style scoped>
+.devise-card {
+
+    width:100%;
+}
+
+.forget-btn {
+    float: right;
+}
+</style>
---
+++ src/components/DeviceList.vue
@@ -0,0 +1,37 @@
+<template>
+    <v-layout row wrap>
+      <v-flex xs12>
+        <v-list >
+            <DeviceItem/>
+            <DeviceItem/>
+        </v-list>
+      </v-flex>
+      <div class="buttons-block">
+        <v-btn>Discover</v-btn>
+        <v-btn>Forget All</v-btn>
+      </div>
+    </v-layout>
+</template>
+
+<script>
+import DeviceItem from './DeviceItem'
+
+export default {
+  name: 'DeviceList',
+  props: {
+    
+  },
+  components: {
+    DeviceItem
+  }
+}
+</script>
+
+<style scoped>
+.buttons-block {
+    display: flex;
+    justify-content: flex-end;
+    width: 100%;
+    
+}
+</style>
